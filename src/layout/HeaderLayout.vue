a46fc2a7879ecf0fb14c6bfae76405ff71b282d2
PTC-NEC\tina.chen <tina.chen@ptc-nec.com>
2024-05-31 17:58:50 +0800

    feat: header

---
+++ src/layout/HeaderLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar flex-space-between">
+    <div class="navbar__logo-container">
+      <img class="navbar__logo" src="@/img/logo-all-dark.svg"/>
+    </div>
+    <div class="navbar__links flex-center">
+      <router-link class="navbar__link" to="/">首頁</router-link>
+      <router-link class="navbar__link" to="/product">甜點</router-link>
+      <router-link class="navbar__link" to="/login">登入</router-link>
+      <router-link class="navbar__link navbar__link--cart" to="/cart">
+        <img class="navbar__cart-icon" src="@/img/shopping_cart_24dp.svg"/>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderLayout'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/HomeView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/product',
+    name: 'product',
+    component: () => import('../views/ProductView.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: () => import('../views/CheckoutView.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: () => import('../views/CartView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
   }
 ]
 
